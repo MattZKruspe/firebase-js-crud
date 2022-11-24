ff0a121e6e09af0098051417a78c62350ce2865e
MattZKruspe <matthewmarquez2003@gmail.com>
2022-11-23 23:11:57 -0600

    fifth commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import { saveTask, getTasks, onGetTasks, deleteTask} from './firebase.js';
+import { saveTask, getTasks, onGetTasks, deleteTask, getTask } from './firebase.js';
 
 const taskForm = document.getElementById('task-form');
 const tasksContainer = document.getElementById('tasks-container');
+
+let editStatus = false;
 
 window.addEventListener('DOMContentLoaded', async () => {
     onGetTasks((querySnapshot) => {
@@ -16,6 +18,7 @@ window.addEventListener('DOMContentLoaded', async () => {
                 <h3>${task.title}</h3>
                 <p>${task.description}</p>
                 <button class="btn-delete" data-id="${doc.id}">Delete</button>
+                <button class="btn-edit" data-id="${doc.id}">Edit</button>
             </div>
             `;
         });
@@ -24,11 +27,29 @@ window.addEventListener('DOMContentLoaded', async () => {
         const btnsDelete = tasksContainer.querySelectorAll('.btn-delete')
 
         btnsDelete.forEach(btn => {
-            btn.addEventListener('click', ({target:{dataset}}) => {
+            btn.addEventListener('click', ({ target: { dataset } }) => {
                 // console.log(dataset.id);
                 deleteTask(dataset.id)
             })
         })
+
+        const btnsEdit = tasksContainer.querySelectorAll('.btn-edit')
+        btnsEdit.forEach(btn => {
+            // console.log(btn)
+            btn.addEventListener('click', async (e) => {
+                // console.log(e.target.dataset.id)
+                const doc = await getTask(e.target.dataset.id)
+                // console.log(doc.data())
+                const task = doc.data()
+
+                taskForm['task-title'].value = task.title;
+                taskForm['task-description'].value = task.description;
+
+                editStatus = true;
+            })
+
+        })
+
     });
 });
 
@@ -37,6 +58,12 @@ taskForm.addEventListener('submit', (e) => {
 
     const title = taskForm['task-title']
     const description = taskForm['task-description']
+
+    if (editStatus) {
+        console.log('updating');
+    } else {
+        saveTask(title.value, description.value);
+    }
 
     saveTask(title.value, description.value)
 
